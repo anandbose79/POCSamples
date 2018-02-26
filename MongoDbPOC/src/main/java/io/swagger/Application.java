6a6da41d5355085595b6062f4c560f3b09b0da76
Anand Bose <anandbose@anands-mbp.attlocal.net>
2018-02-25 18:06:04 -0600

    added

---
+++ MongoDbPOC/src/main/java/io/swagger/Application.java
@@ -0,0 +1,22 @@
+package io.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+@ComponentScan(basePackages = { "io.swagger", "io.swagger.api" })
+public class Application  {
+
+
+    public static void main(String[] args) throws Exception {
+        new SpringApplication(Application.class).run(args);
+    }
+
+
+
+    
+}
